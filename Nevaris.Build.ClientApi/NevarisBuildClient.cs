1595208cd0ca27ae2580da8826b7b1c65cb118dd
Andreas Horwath <andreas.horwath@gmx.eu>
2020-01-08 07:51:10 +0100

    Neu: Methode NevarisBuildClient.CheckVersion zur Versionsprüfung

--- Nevaris.Build.ClientApi/NevarisBuildClient.cs
+++ Nevaris.Build.ClientApi/NevarisBuildClient.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
@@ -47,6 +49,27 @@ namespace Nevaris.Build.ClientApi
         /// Zugriff auf Stammdaten-Operationen.
         /// </summary>
         public IProjektApi ProjektApi { get; }
+
+        /// <summary>
+        /// Ermittelt die Version der HTTP-API und gibt sie gemeinsam mit der Version der Client-Library (Nevaris.Build.ClientApi)
+        /// zurück. Auf Basis des zurückgegebenen Objekts kann überprüft werden, ob API und Client miteinander kompatibel sind.
+        /// </summary>
+        /// <returns>Objekt mit Versionsinformationen</returns>
+        public async Task<VersionCheckResult> CheckVersion()
+        {
+            var apiVersionInfo = await StammApi.GetVersion();
+            string[] apiVersionSegments = apiVersionInfo.ApiVersion.Split('.');
+
+            var apiVersion = new Version(
+                major: apiVersionSegments.Length > 0 ? int.Parse(apiVersionSegments[0], CultureInfo.InvariantCulture) : 0,
+                minor: apiVersionSegments.Length > 1 ? int.Parse(apiVersionSegments[1], CultureInfo.InvariantCulture) : 0,
+                build: apiVersionSegments.Length > 2 ? int.Parse(apiVersionSegments[2], CultureInfo.InvariantCulture) : 0,
+                revision: 0);
+
+            var clientVersion = Assembly.GetExecutingAssembly().GetName().Version;
+
+            return new VersionCheckResult(clientVersion: clientVersion, apiVersion: apiVersion);
+        }
 
         static RefitSettings _refitSettings = new RefitSettings
         {
